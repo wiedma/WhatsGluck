dde6f1ae489a8066cfa71fbdaa90d097128ad706
marco.wiedmann <marco.wiedmann@edv117-05.wgg.local>
2018-06-19 13:58:58 +0200

    Kontakte zu String und zurück konvertieren

--- WhatsGluck/src/org/GUI/Chatfenster.java
+++ WhatsGluck/src/org/GUI/Chatfenster.java
@@ -29,7 +29,7 @@ public class Chatfenster extends JFrame{
 	public static final int KONTAKT_BREITE = 150;
 	public static final int KONTAKT_HOEHE = 50;
 	public static final String STANDART_SENDE_TEXT = "Nachricht schreiben";
-	public static final int MAX_WORTLAENGE = 60;
+	public static final int MAX_WORTLAENGE = 30;
 	
 	public Chatfenster() {
 		//RootPanel 
